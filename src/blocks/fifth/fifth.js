dcabd3d15e16ed6179a7a0781e0a917fe0581ee8
Ruslan Andreev <ruslanandreevpro@gmail.com>
2019-04-27 13:59:09 +0400

    add form validation

--- src/blocks/fifth/fifth.js
+++ src/blocks/fifth/fifth.js
@@ -1,13 +1,54 @@
-modules.define('fifth', ['i-bem-dom'], function(provide, bemDom) {
+const NameField = document.getElementById('name')
+const errName = document.getElementById('errName')
+const EmailField = document.getElementById('email')
+const errEmail = document.getElementById('errEmail')
+const PhoneField = document.getElementById('phone')
+const errPhone = document.getElementById('errPhone')
+const ProjectField = document.getElementById('project')
+const errProject = document.getElementById('errProject')
+const submitForm = document.getElementById('form')
+const submitButton = document.getElementById('submit')
+let isValidName = false
+let isValidEmail = false
+let isValidPhone = false
+let isValidProject = false
+let formValid = false
+const isValid = 'valid'
+const isInvalid = 'invalid'
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+NameField.addEventListener('keyup', (event) => {
+    isValidName = NameField.checkValidity()
+    isValidName ? NameField.className = isValid : NameField.className = isInvalid
+    errName.innerHTML = NameField.validationMessage
+    formValid = isValidName && isValidEmail && isValidPhone && isValidProject
+});
 
+EmailField.addEventListener('keyup', (event) => {
+    isValidEmail = EmailField.checkValidity()
+    isValidEmail ? EmailField.className = isValid : EmailField.className = isInvalid
+    errEmail.innerHTML = EmailField.validationMessage
+    formValid = isValidName && isValidEmail && isValidPhone && isValidProject
+});
+
+PhoneField.addEventListener('keyup', (event) => {
+    isValidPhone = PhoneField.checkValidity()
+    isValidPhone ? PhoneField.className = isValid : PhoneField.className = isInvalid
+    errPhone.innerHTML = PhoneField.validationMessage
+    formValid = isValidName && isValidEmail && isValidPhone && isValidProject
+});
+
+ProjectField.addEventListener('keyup', (event) => {
+    isValidProject = ProjectField.checkValidity()
+    isValidProject ? ProjectField.className = isValid : ProjectField.className = isInvalid
+    errProject.innerHTML = ProjectField.validationMessage
+    formValid = isValidName && isValidEmail && isValidPhone && isValidProject
+});
+
+document.addEventListener('keyup', () => {
+    console.log(isValidName, isValidEmail, isValidPhone, isValidProject, formValid)
+    formValid ? submitButton.disabled = false : submitButton.disabled = true
+})
+
+submitButton.addEventListener('click', (event) => {
+    submitForm.submit();
 });
--- src/js/import/blocks.js
+++ src/js/import/blocks.js
@@ -1,3 +1,4 @@
 import "%blocks%/header/header.js";
 import "%blocks%/footer/footer.js";
 import "%blocks%/first/first.js";
+import "%blocks%/fifth/fifth.js";
